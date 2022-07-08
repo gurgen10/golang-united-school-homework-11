7f505f9d6eec98c754f6c442a5690e086377f8fb
Gurgen Torosyan <gurgen.torosyan@exio.tech>
2022-07-08 17:17:35 +0400

    Done

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -21,34 +21,19 @@ func getBatch(n int64, pool int64) (res []user) {
 	sem := make(chan struct{}, pool)
 	var i int64 = 0
 
-	for ; i < n/pool; i++ {
+	for ; i < n; i++ {
 		wg.Add(1)
 		sem <- struct{}{}
 		
 			go func(id int64) {
-				mx.Lock()
 				user := getOne(id)
+				mx.Lock()
 				res = append(res, user)
 				mx.Unlock()
 				<-sem
 				wg.Done()
 				}(int64(i))	
-			
 		}
-		for i = n/pool; i < n; i++ {
-			wg.Add(1)
-			sem <- struct{}{}
-			
-				go func(id int64) {
-					mx.Lock()
-					user := getOne(id)
-					res = append(res, user)
-					mx.Unlock()
-					<-sem
-					wg.Done()
-					}(int64(i))	
-				
-			}
 			
 		wg.Wait()
 	return res
